fa351e0c31bc7d1b68c970c3d257d96cbccae862
kia martinez <kiawn@MacBook-Pro-3.home>
2025-01-18 09:05:22 -0600

    idk

--- internal/cmdRunners/observability/apply.go
+++ internal/cmdRunners/observability/apply.go
@@ -263,13 +263,7 @@ func apply(ctx context.Context, cfg aws.Config, mm *magicmodel.Operator, account
 
 	err = handleWireguardUpdates(mm, *updatedNetwork, cfg)
 	if err != nil {
-		account.Observability.Status = "APPLY_FAILED"
-		account.Observability.FailedReason = err.Error()
-		o = mm.Save(&account)
-		if o.Err != nil {
-			log.Err(o.Err).Str("JobId", payload.JobId).Msg(o.Err.Error())
-			return nil, o.Err
-		}
+		log.Err(o.Err).Str("JobId", payload.JobId).Msg(o.Err.Error())
 		return nil, err
 	}
 
