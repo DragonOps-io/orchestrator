cc40edcbab905ecfb70324c7b958844aa2c1e20a
kia martinez <kiawn@MacBook-Pro-4.local>
2025-06-03 12:30:54 -0500

    random comment to get this to build again

--- internal/cmdRunners/group/apply.go
+++ internal/cmdRunners/group/apply.go
@@ -135,7 +135,8 @@ func formatWithWorkerAndApply(ctx context.Context, masterAcctRegion string, mm *
 
 	//}
 
-	// template with worker a second time, now that resources are destroyed
+	//// template with worker a second time, now that resources are destroyed
+
 	err := runWorkerGroupApply(mm, group, payload.JobId, masterAcctRegion)
 	if err != nil {
 		return fmt.Errorf("error templating terraform: %v", err)
